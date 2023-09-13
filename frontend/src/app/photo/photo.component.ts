b4987901c4a3968aea5b47b7a1761fa835ae17cb
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-13 07:21:00 -0300

    Usando o @Input

--- frontend/src/app/photo/photo.component.ts
+++ frontend/src/app/photo/photo.component.ts
@@ -1,12 +1,12 @@
 
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector:'app-photo',
   templateUrl:'./photo.component.html'
 })
 export class PhotoComponent{
-  title = 'Angular pic';
-  description = "Imagem do angular"
-  url = "https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png";
+  @Input() title = "";
+  @Input() description = "";
+  @Input() url = "";
 }
