3b9e6811e988a92838b433494c68fde55028f319
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-12 21:22:47 -0300

    First commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular pic';
+  description = "Imagem do angular"
+  url = "https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png";
+}
