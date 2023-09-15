f67290ffbab35b013ea4e0ceeccfe8eff618f445
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-14 22:37:37 -0300

    API

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  photos = [
+    {
+      url:'https://miro.medium.com/v2/resize:fit:1400/1*bMgQ8MhbnQexpqHgIgBJPA.png',
+      description:'Angular'
+    },
+    {
+      url:'https://www.etondigital.com/wp-content/uploads/2019/05/laravel-blog.png',
+      description:'Laravel'
+    },
+  ]
 }
