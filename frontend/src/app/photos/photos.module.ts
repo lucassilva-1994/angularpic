0696c2327ef85e42aa3bc58d68a6151685939f18
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-17 19:05:10 -0300

    Adicionando a interface OnInit

--- frontend/src/app/photos/photos.module.ts
+++ frontend/src/app/photos/photos.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { PhotoComponent } from "./photo/photo.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   //Todos os componentes que vai fazer parte desse module
   declarations: [PhotoComponent],
   //Todos os componentes que vai ficar visivel para o module que for importar esse module.
-  exports:[PhotoComponent]
+  exports:[PhotoComponent],
+  imports:[HttpClientModule]
 })
 export class PhotosModule{
 
