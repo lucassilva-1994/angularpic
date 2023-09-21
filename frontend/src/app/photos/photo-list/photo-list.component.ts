19226feed04de706d42ce702670309814ae61060
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-20 21:23:15 -0300

    Implementando o botão de Load more

--- frontend/src/app/photos/photo-list/photo-list.component.ts
+++ frontend/src/app/photos/photo-list/photo-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -13,10 +14,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor( private activatedRoute: ActivatedRoute){}
+  constructor( private activatedRoute: ActivatedRoute, private photoService: PhotoService){}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
       this.photos = this.activatedRoute.snapshot.data['photos'];
       this.debounce
         .pipe(debounceTime(300))
@@ -25,5 +30,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
    ngOnDestroy(): void {
       this.debounce.unsubscribe();
+  }
+
+  load(){
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe(photos => {
+            this.photos = this.photos.concat(photos);
+            if(!photos.length)
+                this.hasMore = false;
+        });
   }
 }
