536d13fe6d379d4c33813bbab7417cde99a83799
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-23 19:57:13 -0300

    Iniciando o processo de autenticação, enviando credenciais para a API

--- frontend/src/app/home/signin/signin.component.ts
+++ frontend/src/app/home/signin/signin.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../core/auth.service';
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
@@ -7,11 +8,29 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class SignInComponent implements OnInit{
       loginForm: FormGroup;
 
-      constructor(private formBuilder: FormBuilder){}
+      constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService){}
+
       ngOnInit(): void {
           this.loginForm = this.formBuilder.group({
               userName: ['', Validators.required],
               password: ['', Validators.required]
           });
+      }
+
+      login(){
+          const userName = this.loginForm.get('userName').value;
+          const password = this.loginForm.get('password').value;
+          this.authService
+          .authenticate(userName,password)
+          .subscribe(
+            () => console.log('Sucesso.'),
+            err => {
+              console.log('Falha na Autenticação')
+              this.loginForm.reset(),
+              alert('Username or/and password invalid')
+            }
+          )
       }
 }
