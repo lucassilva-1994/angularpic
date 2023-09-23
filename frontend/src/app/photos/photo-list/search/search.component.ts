484ec0f5caed29fca3340b1afc5b0d31ac9bf329
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-22 22:17:45 -0300

    Componentizando o campo de busca (Search)

---
+++ frontend/src/app/photos/photo-list/search/search.component.ts
@@ -0,0 +1,20 @@
+import { debounceTime } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subject } from "rxjs";
+
+@Component({
+  selector:'ap-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent implements OnInit, OnDestroy{
+  debounce: Subject<string> = new Subject<string>();
+
+  ngOnInit(): void {
+    this.debounce
+    .pipe(debounceTime(300));
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+}
