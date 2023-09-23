98664bd89f1ab2beb436820fb00a12fc50fee881
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-22 23:29:19 -0300

    Finalizando a diretiva (DarkenOnHover), e usando o HostListener

--- frontend/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ frontend/src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,8 +1,19 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input, Renderer } from "@angular/core";
 
 @Directive({
     selector:'[apDarkenOnHover]'
 })
 export class DarkenOnHoverDirective{
+      constructor(private el: ElementRef,  private render: Renderer){}
 
+      //Informar como parametro o nome do evento
+      @HostListener('mouseover')
+      darkenOn(){
+          this.render.setElementStyle(this.el.nativeElement,'filter',`brightness(70%)`);
+      }
+
+      @HostListener('mouseleave')
+      darkenOff(){
+        this.render.setElementStyle(this.el.nativeElement,'filter',`brightness(100%)`);
+      }
 }
