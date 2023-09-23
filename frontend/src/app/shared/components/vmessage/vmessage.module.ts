a435f92f109df505638d5eb02f5e95dec21c6657
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-23 19:36:47 -0300

    Componentizando a mensagem de erro

---
+++ frontend/src/app/shared/components/vmessage/vmessage.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { VMessageComponent } from "./vmessagecomponent";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    declarations:[VMessageComponent],
+    exports: [VMessageComponent]
+})
+export class VMessageModule{
+
+}
