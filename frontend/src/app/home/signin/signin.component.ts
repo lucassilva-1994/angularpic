50298a510761b0632873ec6a5e702e6fab64839b
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-23 19:20:13 -0300

    Validando o formulário

--- frontend/src/app/home/signin/signin.component.ts
+++ frontend/src/app/home/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   templateUrl: './signin.component.html'
@@ -10,8 +10,8 @@ export class SignInComponent implements OnInit{
       constructor(private formBuilder: FormBuilder){}
       ngOnInit(): void {
           this.loginForm = this.formBuilder.group({
-              userName: ['Lucas'],
-              password: ['123']
+              userName: ['', Validators.required],
+              password: ['', Validators.required]
           });
       }
 }
