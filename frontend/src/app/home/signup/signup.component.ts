231565532e176929b71aafc6ddc15103f16ba5d5
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-24 19:04:54 -0300

    Validação assíncrono (Verificando se o usuário já existe)

--- frontend/src/app/home/signup/signup.component.ts
+++ frontend/src/app/home/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { lowerCaseValidator } from "src/app/shared/validators/lower-case.validator";
+import { UserNotTakenValidatorService } from "./user-not-taken.validator.service";
 
 @Component({
   templateUrl: './signup.component.html'
@@ -8,7 +9,9 @@ import { lowerCaseValidator } from "src/app/shared/validators/lower-case.validat
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService) { }
 
   ngOnInit(): void {
     this.signUpForm = this.formBuilder.group({
@@ -31,7 +34,8 @@ export class SignUpComponent implements OnInit {
           lowerCaseValidator,
           Validators.minLength(2),
           Validators.maxLength(30)
-        ]
+        ],
+        this.userNotTakenValidatorService.checkUserNameTaken()
       ],
       password: ['',
         [
