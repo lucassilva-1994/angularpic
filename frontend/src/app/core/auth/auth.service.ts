44b5c8536bed7bf8e0e94413518cca8e510cd8b0
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-23 22:26:25 -0300

    Criando serviço para tratamento do token

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 @Injectable({
@@ -8,7 +9,9 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string){
       return this.http.post(
@@ -17,6 +20,7 @@ export class AuthService {
         {observe : 'response'})
         .pipe(tap(res => {
           const authToken = res.headers.get('x-access-token');
+          this.tokenService.setToken(authToken);
           console.log(`User ${userName} authenticated with token ${authToken}`)
         }));
   }
