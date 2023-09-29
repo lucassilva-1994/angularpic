2b35ebc8154593b1ec7d38c9c9ac3017d8d037a3
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-28 21:34:00 -0300

    Iniciando o componente Alert

---
+++ frontend/src/app/shared/components/alert/alert.ts
@@ -0,0 +1,13 @@
+export class Alert{
+    constructor(
+        private readonly alertType: AlertType,
+        private readonly message: string
+    ){}
+}
+
+export enum AlertType{
+    SUCCESS,
+    WARNING,
+    DANGER,
+    INFO
+}
