6575c1caee5f44ab577a7971e6cd46b5f1b9ec8a
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-30 10:17:57 -0300

    Ocultando a alerta quando o usuário acessar a próxima rota depois do redirecionamento feito pela aplicação

--- frontend/src/app/photos/photo-detail/photo-details.component.ts
+++ frontend/src/app/photos/photo-detail/photo-details.component.ts
@@ -4,6 +4,7 @@ import { PhotoService } from "../photo/photo.service";
 import { Photo } from "../photo/photo";
 import { Observable } from "rxjs";
 import { AlertService } from "src/app/shared/components/alert/alert.service";
+import { UserService } from "src/app/core/user/user.service";
 
 @Component({
     templateUrl: './photo-details.component.html'
@@ -15,7 +16,8 @@ export class PhotoDetailsComponent implements OnInit{
         private route: ActivatedRoute,
         private photoService: PhotoService,
         private router: Router,
-        private alertService: AlertService
+        private alertService: AlertService,
+        private userService: UserService
     ){}
 
     ngOnInit(): void { 
@@ -27,12 +29,12 @@ export class PhotoDetailsComponent implements OnInit{
         this.photoService
             .removePhoto(this.photoId)
             .subscribe(()=> {
-                this.alertService.success("Photo removed!");
-                this.router.navigate(['']);
+                this.alertService.success("Photo removed!", true);
+                this.router.navigate(['/user', this.userService.getUserName()]);
               },
               error => {
                 console.log(error);
-                this.alertService.warning("Could not delete the photo!");
+                this.alertService.warning("Could not delete the photo!", true);
               }
             );
     }
