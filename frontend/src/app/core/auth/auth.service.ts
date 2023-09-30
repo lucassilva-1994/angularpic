283664806edcdd6fd2338ed7fea11845b4b87126
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-09-30 15:47:48 -0300

    Definindo a URL da API em uma váriavel de ambiente

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -2,8 +2,10 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
 import { UserService } from '../user/user.service';
+import { environment } from 'src/environments/environment';
 
-const API_URL = 'http://localhost:3000';
+
+const API = environment.apiUrl;
 @Injectable({
   providedIn: 'root'
 })
@@ -15,7 +17,7 @@ export class AuthService {
 
   authenticate(userName: string, password: string){
       return this.http.post(
-        API_URL + '/user/login',
+        API + '/user/login',
         {userName, password},
         {observe : 'response'})
         .pipe(tap(res => {
