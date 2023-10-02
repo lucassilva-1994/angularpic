4e5a4d93f031479e87f1c5148f1ae432e91ad014
Lucas Sílva <lucas.silva_1994@outlook.com>
2023-10-01 21:10:38 -0300

    Tratando as informações de erros que será enviado para o backend

--- frontend/src/app/errors/global-error-handler/global-error-handler.ts
+++ frontend/src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,16 +1,31 @@
-import { ErrorHandler } from "@angular/core";
+import { LocationStrategy, PathLocationStrategy } from "@angular/common";
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { UserService } from "src/app/core/user/user.service";
 import * as StackTrace from 'stacktrace-js';
 
+@Injectable()
 export class GlobalErrorHandler implements ErrorHandler{
+    constructor(private injector: Injector){}
     handleError(error: any): void {
-        console.log("Passei pelo handler.")
+        console.log("Passei pelo handler.");
+        const location = this.injector.get(LocationStrategy);
+        const userService = this.injector.get(UserService);
+        const url = location instanceof PathLocationStrategy
+            ? location.path()
+            : '';
+        const message = error.message
+            ? error.message
+            : error.toString();
         StackTrace
             .fromError(error)
             .then(stackFrames =>{
                 const stackAsString = stackFrames
                       .map(sf => sf.toString())
                       .join('\n');
+                    console.log(message);
                     console.log(stackAsString);
+                    console.log('O que será enviado para o servidor');
+                    console.log({message, url, userName: userService.getUserName() ,stack: stackAsString});
             });
     }
 }
